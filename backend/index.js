1194ab3d07291cc812b1ad522249f2cf6b88ea48
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2022-12-17 09:36:19 -0500

    changed port num

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = 3001;
 const axios = require("axios");
 const ticketRecordModel = require("./TicketRecordModels");
 
